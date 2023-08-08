45ef46eece19c62fc3793eb96fb2159c1d0edcc1
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-08-08 17:55:39 +0500

    create

--- src/pages/branche/create.vue
+++ src/pages/branche/create.vue
@@ -1,13 +1,25 @@
-<template lang="">
+<template >
     <div>
-        
+
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            location: '',
+            name: ''
+        }
+    },
+    methods: {
+        createBranche() {
+            const options = {
+                location: this.location,
+                name: this.name
+            }
+            this.$store.dispatch('branche/createBreanch', options)
+        }
+    }
 }
 </script>
-<style lang="">
-    
-</style>
+<style></style>
--- src/store/modules/branche.js
+++ src/store/modules/branche.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+const token = localStorage.getItem("token");
 
 let state = {
   branchData: [],
@@ -11,15 +12,22 @@ const mutations = {
 const actions = {
   async getBranches({ commit }) {
     commit("setLoading", true, { root: true });
-    const token = localStorage.getItem("token");
     try {
       let response = await axios.get("http://tulibayev.uz/api/branch", {
         headers: { Authorization: "Bearer " + token },
       });
       // console.log(response.data);
       commit("setBranch", response.data.data);
-    commit("setLoading", false, { root: true });
-
+      commit("setLoading", false, { root: true });
+    } catch (error) {
+      console.error("error find", error);
+    }
+  },
+  async createBreanch({ commit },option) {
+    try {
+      const response = await axios.post("http://tulibayev/api/branch", {
+        headers: { Authorization: "Bearer " + token },
+      });
     } catch (error) {
       console.error("error find", error);
     }
