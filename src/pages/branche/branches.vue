11cf4ce8b0330b8516cc8e0ba23f30c8b6108917
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-08-09 16:41:33 +0500

    problem with router

--- src/pages/branche/branches.vue
+++ src/pages/branche/branches.vue
@@ -5,70 +5,81 @@
         <div class="container-fluid ">
             <div class="row">
                 <div class="col-xl-6 set-col-12 box-col-12">
-                    <div class="card" style="height: 93%;">
-                        <div class="blog-box blog-shadow" style="height: 100%;">
-                            <img class="img-fluid bg-img-cover" style="height: 100%;" src="@/assets/images/it-park.jpg"
-                                alt="">
-                            <div class="blog-details">
-                                <p v-if="branchData && branchData.length">{{ branchData[0].name }}</p>
-                                <p v-else>No branch data available.</p>
-                                <h4>Main Office</h4>
-                                <ul class="blog-social">
-                                    <li><i class="icofont icofont-user"></i>Mark Jecno</li>
-                                    <li><i class="icofont icofont-thumbs-up"></i>02 Hits</li>
-                                    <li><i class="icofont icofont-ui-chat"></i>598 Comments</li>
-                                </ul>
+                    <RouterLink v-if="hasBranchData" :to="{ path: 'branchInfo/' + branchData[0].id }">
+                        <div class="card" style="height: 93%;">
+                            <div class="blog-box blog-shadow" style="height: 100%;">
+                                <img class="img-fluid bg-img-cover" style="height: 100%;" src="@/assets/images/it-park.jpg"
+                                    alt="">
+                                <div class="blog-details">
+                                    <p v-if="branchData && branchData.length">{{ branchData[0].name }}</p>
+                                    <p v-else>No branch data available.</p>
+                                    <h4>Main Office</h4>
+                                    <ul class="blog-social">
+                                        <li><i class="icofont icofont-user"></i>Mark Jecno</li>
+                                        <li><i class="icofont icofont-thumbs-up"></i>02 Hits</li>
+                                        <li><i class="icofont icofont-ui-chat"></i>598 Comments</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
+
+                    </RouterLink>
+
                 </div>
                 <div class="col-xl-6 set-col-12 box-col-12">
-                    <div class="card">
-                        <div class="blog-box blog-list row">
-                            <div class="col-sm-5"><img class="img-fluid sm-100-w" src="@/assets/images/it-park.jpg" alt="">
-                            </div>
-                            <div class="col-sm-7">
-                                <div class="blog-details">
-                                    <div class="blog-date"><span>02</span> January 2018</div>
-                                    <p v-if="branchData && branchData.length">{{ branchData[1].name }}</p>
-                                    <p v-else>No branch data available.</p>
-                                    <div class="blog-bottom-content">
-                                        <ul class="blog-social">
-                                            <li>by: Admin</li>
-                                            <li>0 Hits</li>
-                                        </ul>
-                                        <hr>
-                                        <p class="mt-0">inventore veritatis et quasi architecto beatae vitae dicta sunt
-                                            explicabo. Nemo enim ipsam voluptatem quia voluptas sit.</p>
+                    <RouterLink v-if="hasBranchData" :to="{ path: 'branchInfo/' + branchData[1].id }">
+                        <div class="card">
+                            <div class="blog-box blog-list row">
+                                <div class="col-sm-5"><img class="img-fluid sm-100-w" src="@/assets/images/it-park.jpg"
+                                        alt="">
+                                </div>
+                                <div class="col-sm-7">
+                                    <div class="blog-details">
+                                        <div class="blog-date"><span>02</span> January 2018</div>
+                                        <p v-if="branchData && branchData.length">{{ branchData[1].name }}</p>
+                                        <p v-else>No branch data available.</p>
+                                        <div class="blog-bottom-content">
+                                            <ul class="blog-social">
+                                                <li>by: Admin</li>
+                                                <li>0 Hits</li>
+                                            </ul>
+                                            <hr>
+                                            <p class="mt-0">inventore veritatis et quasi architecto beatae vitae dicta sunt
+                                                explicabo. Nemo enim ipsam voluptatem quia voluptas sit.</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card">
-                        <div class="blog-box blog-list row">
-                            <div class="col-sm-5"><img class="img-fluid sm-100-w" src="@/assets/images/it-park.jpg" alt="">
-                            </div>
-                            <div class="col-sm-7">
-                                <div class="blog-details">
-                                    <div class="blog-date"><span>03</span> January 2018</div>
-                                    <p v-if="branchData && branchData.length">{{ branchData[2].name }}</p>
-                                    <p v-else>No branch data available.</p>
-                                    <div class="blog-bottom-content">
-                                        <ul class="blog-social">
-                                            <li>by: Admin</li>
-                                            <li>02 Hits</li>
-                                        </ul>
-                                        <hr>
-                                        <p class="mt-0">inventore veritatis et quasi architecto beatae vitae dicta sunt
-                                            explicabo. Nemo enim ipsam voluptatem quia voluptas sit.</p>
+                    </RouterLink>
+                    <RouterLink v-if="hasBranchData" :to="{ path: 'branchInfo/' + branchData[2].id }">
+
+                        <div class="card">
+                            <div class="blog-box blog-list row">
+                                <div class="col-sm-5"><img class="img-fluid sm-100-w" src="@/assets/images/it-park.jpg"
+                                        alt="">
+                                </div>
+                                <div class="col-sm-7">
+                                    <div class="blog-details">
+                                        <div class="blog-date"><span>03</span> January 2018</div>
+                                        <p v-if="branchData && branchData.length">{{ branchData[2].name }}</p>
+                                        <p v-else>No branch data available.</p>
+                                        <div class="blog-bottom-content">
+                                            <ul class="blog-social">
+                                                <li>by: Admin</li>
+                                                <li>02 Hits</li>
+                                            </ul>
+                                            <hr>
+                                            <p class="mt-0">inventore veritatis et quasi architecto beatae vitae dicta sunt
+                                                explicabo. Nemo enim ipsam voluptatem quia voluptas sit.</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </RouterLink>
                 </div>
-                <div class="col-md-6 col-xl-3 box-col-6 "  v-for='item in branchData' :key="item" >
+                <div class="col-md-6 col-xl-3 box-col-6 " v-for='item in branchData' :key="item">
                     <div class="card">
                         <div class="blog-box blog-grid text-center">
                             <div class="blog-wrraper"><a href="blog-single.html">
@@ -86,6 +97,15 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-md-6 col-xl-3 box-col-6  select">
+                    <RouterLink to="branch/create">
+                        <div class="card">
+                            <div class="blog-box blog-grid text-center hover">
+                                <i class='bx bx-plus-circle'></i>
+                            </div>
+                        </div>
+                    </RouterLink>
+                </div>
             </div>
         </div>
     </div>
@@ -98,7 +118,10 @@ export default {
         spiner,
     },
     computed: {
-        ...mapState('branche', ['branchData'])
+        ...mapState('branche', ['branchData']),
+        hasBranchData() {
+            return this.branchData && this.branchData.length > 0;
+        }
     },
     mounted() {
         this.branch()
@@ -117,5 +140,15 @@ export default {
     height: auto;
     display: flex;
     flex-direction: column;
+}
+
+.hover {
+    height: 340px;
+    line-height: 340px;
+    font-size: 50px;
+}
+
+.select:hover {
+    transform: scale(1.050);
 }
 </style>
