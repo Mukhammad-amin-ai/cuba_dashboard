a65c2718d368046eeeb7895b9f4f390089f4a5ab
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-08-28 16:38:29 +0500

    getCaashiers id

--- src/store/modules/payme.js
+++ src/store/modules/payme.js
@@ -3,7 +3,8 @@ const token = localStorage.getItem("token");
 const state = {
   showHide: true,
   showVerify: true,
-  addCardComponent:true
+  addCardComponent: true,
+  cashierId: "",
 };
 const mutations = {
   setShow(state, showHide) {
@@ -12,27 +13,42 @@ const mutations = {
   setShowVerify(state, showVerify) {
     state.showVerify = showVerify;
   },
-  cardComponent(state,addCardComponent){
-    state.addCardComponent = addCardComponent
-  }
+  cardComponent(state, addCardComponent) {
+    state.addCardComponent = addCardComponent;
+  },
+  getId(state, cashierId) {
+    state.cashierId = cashierId;
+  },
 };
 
-
-
 const actions = {
-  async getCard({ commit }, option) {
+  async getCashiersId({ commit }) {
+    try {
+      let responce = await axios.get(
+        "http://tulibayev.uz/api/payment/cashier",
+        { headers: { Authorization: "Bearer " + token } }
+      );
+      console.log(responce.data);
+      if (responce.data.cashier_id) {
+        commit("getId", responce.data.cashier_id);
+      }
+    } catch (e) {
+      console.error("problem with getting id", e);
+    }
+  },
+  async getCard({ commit, dispatch }, option) {
+    await dispatch('getCashiersId');
     try {
       let response = await axios.post(
         "https://checkout.test.paycom.uz/api",
         option,
         {
           headers: {
-            "X-Auth": "5e730e8e0b852a417aa49ceb ",
+            "X-Auth": state.cashierId,
           },
         }
       );
       console.log(response.data);
-      // commit('cardComponent',false)
       if (response.data.result.card.token) {
         let token = response.data.result.card.token;
         localStorage.setItem("tokenPayme", token);
@@ -70,30 +86,25 @@ const actions = {
         }
       );
       console.log(response.data);
-      // if (response.data) {
-      //   let tokenPayme = localStorage.getItem("tokenPayme");
-      //   let sentoption = {
-      //       type: "student",
-      //       id: 1,
-      //       payment_token: tokenPayme
-      //   }
-      //   sendToken(sentoption);
-      // }
     } catch (e) {
       console.error("error there", e);
     }
   },
-  async sendToken({commit},option) {
+  async sendToken({ commit }, option) {
     try {
-      let responce = await axios.post("http://tulibayev.uz/api/payment/addcard", option, {
-        headers: { Authorization: "Bearer" + token },
-      });
+      let responce = await axios.post(
+        "http://tulibayev.uz/api/payment/addcard",
+        option,
+        {
+          headers: { Authorization: "Bearer" + token },
+        }
+      );
       console.log(responce.data);
-      commit('cardComponent',false)
+      commit("cardComponent", false);
     } catch (e) {
       console.error("error in sending token ", e);
     }
-  }
+  },
 };
 
 export default {
