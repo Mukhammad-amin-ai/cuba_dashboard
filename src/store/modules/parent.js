d6c712bd5ef5a2ece48a7d8eac21818eba77fa2a
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-09-13 17:42:11 +0500

    fix

---
+++ src/store/modules/parent.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+import Api from "./Base_Url";
+
+const token = localStorage.getItem("token");
+
+
+
+let state = {
+
+}
+const mutations = {
+
+}
+const actions = {
+
+}
+
+export default {
+    namespaced:true,
+    state,
+    mutations,
+    actions
+}
