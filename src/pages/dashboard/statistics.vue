96dcc732c27bcdd15c02c6f3df29939cefc9ac9b
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-09-20 16:07:30 +0500

    updated version of statistics

--- src/pages/dashboard/statistics.vue
+++ src/pages/dashboard/statistics.vue
@@ -1,22 +1,61 @@
 <template >
-    <div class="cover">
-        
+    <div class="cover ">
+        <div class="card2">
+            <div class="left-text">
+                <h4>{{ statisticsOfBranch }}</h4>
+                <span class="f-light">
+                    <!-- {{ puechaseSales.title }} -->
+                    {{ nameOf }}
+                </span>
+            </div>
+            <div class="right-img">
+                <img :src="link2" alt="#"> 
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
     props: {
-        statisticsOfBranch: String
-    }
+        statisticsOfBranch: String,
+        nameOf: String,
+        link2: String
+    },
+
 }
 </script>
 <style scoped>
-.cover{
+.cover {
     width: 23%;
-    height: 80px;
+    height: 100px;
     border-radius: 10px;
-    background-color: aqua;
     margin-bottom: 10px;
 }
 
+.card2 {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    background-color: #fff;
+    border-radius: 10px;
+    justify-content: space-between;
+    padding: 10px;
+}
+
+.left-text {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+.right-img {
+    width: 50%;
+    height: 100%;
+}
+.right-img img{
+    width: 100%;
+    height: 100%; 
+}   
 </style>
--- src/pages/dashboard/view.vue
+++ src/pages/dashboard/view.vue
@@ -13,16 +13,20 @@
                     <button type="button" class="btn btn-success">Month</button>
                 </div>
             </div>
-            <div class="container-fluid d-flex justify-content-between">
-                <statisticsVue :statisticsOfBranch="statictics" />
-                <statisticsVue :statisticsOfBranch="statictics" />
-                <statisticsVue :statisticsOfBranch="statictics" />
-                <statisticsVue :statisticsOfBranch="statictics" />
-                <statisticsVue :statisticsOfBranch="statictics" />
-                <statisticsVue :statisticsOfBranch="statictics" />
-                <statisticsVue :statisticsOfBranch="statictics" />
-                <statisticsVue :statisticsOfBranch="statictics" />
-
+            <div class="container-fluid d-flex">
+                <statisticsVue :statisticsOfBranch="statictics ? statictics.roles : 'Loading...'" nameOf="Role"
+                    link2="https://img.freepik.com/premium-photo/beautiful-cartoon-character-sitting-comfortable-red-chair-with-laptop-bearded-businessman-suit-3d-illustration_578102-3355.jpg?w=740" />
+                <statisticsVue :statisticsOfBranch="statictics ? statictics.teachers : 'Loading...'" nameOf="Teachers"
+                    link2="https://img.freepik.com/premium-photo/cartoon-male-teacher-isolated-white-background-vector-illustration-ai-generated_894218-552.jpg?w=1060" />
+                <statisticsVue :statisticsOfBranch="statictics ? statictics.assistant_teachers : 'Loading...'"
+                    nameOf="Assistant Teachers"
+                    link2="https://img.freepik.com/premium-photo/photo-business-woman-with-blank-flipchart-3d-illustration-cartoon-style-generative-ai_742418-2951.jpg?w=740" />
+                <statisticsVue :statisticsOfBranch="statictics ? statictics.groups : 'Loading...'" nameOf="Groups"
+                    link2="https://img.freepik.com/premium-vector/young-woman-jane-holding-book-pencil-3d-vector-people-character-illustration_365941-673.jpg?w=740" />
+                <statisticsVue :statisticsOfBranch="statictics ? statictics.courses : 'Loading...'" nameOf="Courses"
+                    link2="https://img.freepik.com/free-psd/3d-nft-icon-digital-artist-male_629802-8.jpg?w=740&t=st=1695205406~exp=1695206006~hmac=892ea7a326dd48776efa73bed71bcfb7ed89f42c78ba5cb7b52978acebe39f65" />
+                <statisticsVue :statisticsOfBranch="statictics ? statictics.stparents : 'Loading...'" nameOf="Parents"
+                    link2="https://img.freepik.com/premium-photo/3d-cartoon-father-son-holding-balloons_567739-6558.jpg?w=900" />
             </div>
         </div>
     </div>
@@ -45,7 +49,8 @@ export default {
     data() {
         return {
             branch: '',
-            statictics: ""
+            statictics: "",
+            link: '../../assets/images/dashboard-3/bg.jpg'
         }
     },
     computed: {
@@ -77,16 +82,20 @@ export default {
 </script>
 <style scoped>
 .d-flex {
+    /* height:; */
     flex-wrap: wrap;
+    gap: 30px;
 }
-.col{
+
+.col {
     width: 100px;
     height: 200px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
 }
-.btn{
+
+.btn {
     padding: 10px;
 }
 </style>
